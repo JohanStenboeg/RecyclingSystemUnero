b356662c8fb807dfeae136cf5435c904f6151516
Captain <mads130f@edu.easj.dk>
2018-03-13 14:05:03 +0100

    total value done

--- src/HanYo/PantVærdier.java
+++ src/HanYo/PantVærdier.java
@@ -1,10 +1,12 @@
 package HanYo;
 
 public class PantVærdier {
+    //Hvor meget de er værd
     private double pantA = 1;
     private double pantB = 1.5;
     private double pantC = 3;
 
+    //Gettere
     public double getPantA() {
         return pantA;
     }
---
+++ src/HanYo/RunApplication.java
@@ -0,0 +1,19 @@
+package HanYo;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RunApplication extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("HanYo/GUI/GuiTil.fxml"));//Henter vores gui fra sample.fxml        primaryStage.setTitle("Panteren!");
+        primaryStage.setTitle("Panteren!");
+        primaryStage.setScene(new Scene(root,600,600));
+        primaryStage.show();
+    }
+}
