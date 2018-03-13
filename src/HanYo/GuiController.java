611830fd1f996099ce77e0856fb216b7477c7ed6
Captain <mads130f@edu.easj.dk>
2018-03-13 14:55:02 +0100

    Alt virker borset fra det johan burde lave eksdeee

--- src/HanYo/GuiController.java
+++ src/HanYo/GuiController.java
@@ -29,6 +29,7 @@ public class GuiController {
     @FXML
     private Label lbl_kronarz;
 
+    TotalValue totalValue = new TotalValue();
 
     @FXML
     private void handleButtonAction(ActionEvent e) {
@@ -36,30 +37,43 @@ public class GuiController {
         //Man ligger en ting ind med Pant A.
         if (e.getSource()==btn_pantA){
             System.out.println("Pant A");
+            totalValue.addOnePantItem(EnumPantTyper.pantA);
+            lbl_kronarz.setText(totalValue.totalAmountOfMoneyYouGet+" kr.");
 
         }
 
         //Man ligger en ting ind med Pant B.
         if (e.getSource()==btn_pantB){
             System.out.println("Pant B");
+            totalValue.addOnePantItem(EnumPantTyper.pantB);
+            lbl_kronarz.setText(totalValue.totalAmountOfMoneyYouGet+" kr.");
+
+
         }
 
         //Man ligger en ting ind med Pant C.
         if (e.getSource()==btn_pantC){
             System.out.println("Pant C");
+            totalValue.addOnePantItem(EnumPantTyper.pantC);
+            lbl_kronarz.setText(totalValue.totalAmountOfMoneyYouGet+" kr.");
         }
 
 
         //Printer total i danske donalders aka. pant
         if (e.getSource()==btn_getPant){
             System.out.println("Getter panteren");
+            lbl_kronarz.setText("Du har nu doneret til os");
+            totalValue.resetAlleVærdier();
+
         }
 
 
         //Laver DB.
         if (e.getSource()==btn_CreateDB){
             System.out.println("Lavede en db");
+
         }
+
 
 
     }
