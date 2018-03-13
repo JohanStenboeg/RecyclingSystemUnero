f65a3f1c8bea601500208d5303d870cb6675f223
Captain <mads130f@edu.easj.dk>
2018-03-13 13:38:52 +0100

    total value done

--- src/HanYo/TotalValue.java
+++ src/HanYo/TotalValue.java
@@ -5,12 +5,50 @@ public class TotalValue {
   int antalB = 0;
   int antalC = 0;
 
-    TotalValue(int antalA,int antalB, int antalC){
+  double totalAmountOfMoneyYouGet = 0;
+
+  PantVærdier pantVærdier = new PantVærdier();
+
+  //Når man panter et item.
+  void addOnePantItem(EnumPantTyper typer){
+      if(typer == EnumPantTyper.pantA){
+          totalAmountOfMoneyYouGet += pantVærdier.getPantA();
+          antalA++;
+      }
+      if(typer == EnumPantTyper.pantB){
+          totalAmountOfMoneyYouGet += pantVærdier.getPantB();
+          antalB++;
+      }
+      if(typer == EnumPantTyper.pantC){
+          totalAmountOfMoneyYouGet += pantVærdier.getPantC();
+          antalC++;
+      }
+
+  }
+
+  //Gettere af antal type.
+    public int getAntalA() {
+        return antalA;
     }
 
+    public int getAntalB() {
+        return antalB;
+    }
+
+    public int getAntalC() {
+        return antalC;
+    }
+
+    public double getTotalAmountOfMoneyYouGet(){
+        return totalAmountOfMoneyYouGet;
+    }
+
+
+    //Resetter værdier, (når man slår på print)
     void resetAlleVærdier() {
         antalA = 0;
         antalB = 0;
         antalC = 0;
+        totalAmountOfMoneyYouGet = 0;
     }
 }
