f84dda463d8bac54d90fb2347ab631908e2c3c26
Johan <Joha6163@edu.easj.dk>
2018-03-13 14:03:33 +0100

    Fjernet bottle og testclass

---
+++ src/HanYo/InsertingBottle.java
@@ -0,0 +1,11 @@
+package HanYo;
+
+import static HanYo.EnumPantTyper.pantA;
+
+public class InsertingBottle {
+    //Når man insætter en flaske skal den læse om det er pant A, pant B eller pant C.
+
+    public void insertBottle(){
+        if()
+    }
+}
