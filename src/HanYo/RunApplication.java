3273034aafe4cd93ea9b6019f5ff535902cf908a
Captain <mads130f@edu.easj.dk>
2018-03-21 14:35:13 +0100

    Burde være done nu, har ikke testet, da jeg er for doven til at installere jdbc

--- src/HanYo/RunApplication.java
+++ src/HanYo/RunApplication.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
@@ -16,8 +17,8 @@ public class RunApplication extends Application{
         //connector
         Connection conn = mySQL_DBconnector.connect();
 
-
-
+        //Ved fejl slet næste linje
+        primaryStage.getIcons().add(new Image("files/f2u_squidward_dab_sticker_by_fluffybunny321-day9vfu.png"));
         Parent root = FXMLLoader.load(getClass().getResource("../GUI/GuiTil.fxml"));//Henter vores gui fra sample.fxml        primaryStage.setTitle("Panteren!");
         primaryStage.setTitle("Panteren!");
         primaryStage.setScene(new Scene(root,600,400));
